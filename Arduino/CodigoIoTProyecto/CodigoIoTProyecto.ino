9080f0d2d939419ccc45c937fa705b3b8eafb2b6
JuanMa Lovera <jma.lovera10@uniandes.edu.co>
2017-08-27 14:48:00 -0500

    Documentación sensor de temperatura

--- Arduino/CodigoIoTProyecto/CodigoIoTProyecto.ino
+++ Arduino/CodigoIoTProyecto/CodigoIoTProyecto.ino
@@ -1,13 +1,17 @@
+//Inclusión de librerías
 #include "DHT.h"
 
+//Definición de pines para los sensores
 #define DHTPIN 2
 
+//Definición de sensor de temperatura 
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup(){
+  //Inicialización del puerto serial para imprimir en monitor
   Serial.begin(9600);
+  //Inicialización de la instancia del sensor de temperatura
   dht.begin();
 }
 
