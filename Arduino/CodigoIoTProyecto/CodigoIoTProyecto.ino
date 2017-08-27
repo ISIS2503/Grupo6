280b501bfc89fce67abccd7162a2f74e467e9559
JuanMa Lovera <jma.lovera10@uniandes.edu.co>
2017-08-27 17:35:38 -0500

    Update of sound sensor

--- Arduino/CodigoIoTProyecto/CodigoIoTProyecto.ino
+++ Arduino/CodigoIoTProyecto/CodigoIoTProyecto.ino
@@ -5,43 +5,50 @@
 #include <Adafruit_TSL2561_U.h>
 
 //Definición de pines para los sensores
-#define DHTPIN 2
+#define DHTPIN 7
 
-//Definición del sensor de temperatura 
+//Definición del sensor de temperatura
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-//Definición del sensor de luminosidad 
+//Definición del sensor de luminosidad
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
-void setup(){
+const int audioPin = A3;
+
+float audioIntensity = 0;
+
+void setup() {
   //Inicialización del puerto serial para imprimir en monitor
   Serial.begin(9600);
   //Inicialización de la instancia del sensor de temperatura
   dht.begin();
 
-  if(!tsl.begin())
+  if (!tsl.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
     Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1);
   }
   /* Setup the sensor gain and integration time */
   configureSensor();
+
 }
 
 void loop()
 {
   float t = dht.readTemperature();
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
+  //Serial.print("Temperature: ");
+  //Serial.print(t);
+  //Serial.print(" *C ");
+  //Serial.println("");
 
   sensors_event_t event;
   tsl.getEvent(&event);
   if (event.light)
   {
-    Serial.print(event.light); Serial.println(" lux");
+    //Serial.print("Luminosidad: ");
+    //Serial.print(event.light); Serial.println(" lux");
   }
   else
   {
@@ -49,6 +56,11 @@ void loop()
        and no reliable data could be generated! */
     Serial.println("Sensor overload");
   }
+  audioIntensity = analogRead (audioPin) * (5.0 / 1023.0); 
+  //Serial.print("Ruido: ");
+  Serial.println(audioIntensity*10);
+  
+  //delay(2000);
 }
 
 //Método que configura el sensor de iluminación
@@ -58,7 +70,7 @@ void configureSensor(void)
   // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
   // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
   tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
-  
+
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
   tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
