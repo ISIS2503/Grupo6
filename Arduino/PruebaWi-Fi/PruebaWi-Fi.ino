24eb1714e2e384c20f00be8f887f994e373179dd
JuanMa Lovera <jma.lovera10@uniandes.edu.co>
2017-09-26 13:55:16 -0500

    Test cases aproximation

--- Arduino/PruebaWi-Fi/PruebaWi-Fi.ino
+++ Arduino/PruebaWi-Fi/PruebaWi-Fi.ino
@@ -1,61 +1,22 @@
-#include <Bridge.h>
-#include <Console.h>
-#include <FileIO.h>
-#include <HttpClient.h>
-#include <Mailbox.h>
-#include <Process.h>
-#include <YunClient.h>
-#include <YunServer.h>
+#include "DHT.h"
 
-#include <SPI.h>
-//char server[] = "192,168,43,254"; // IMPORTANT: If you are using XAMPP you will have to find out the IP address of your computer and put it here (it is explained in previous article). If you have a web page, enter its address (ie. "www.yourwebpage.com")
-IPAddress server(157,253,210,190);
-//IPAddress ip(192,168,43,254);
-YunClient client;
+//Definición de pines para los sensores
+#define DHTPIN 7
+
+//Definición del sensor de temperatura
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-
-  // Serial.begin starts the serial connection between computer and Arduino
-  Serial.begin(9600);
-  Bridge.begin();
-  Serial.println("Inicio");
-
+  /*Serial.begin(9600);
+  dht.begin();*/
 }
 
 void loop() {
-  if (client.connect(server, 80))
-  {
-    Serial.println("connected");
-    String valor = "{\n\t\"data\": 10,\n\t\"unit\": C,\n\t\"place\": \"AREA 1\"\n}";
-    client.println("POST /valor HTTP/1.1");
-    // client.print("Content-length:");
-    // client.println(sensorvalue1.length());
-    //  client.println(sensorvalue2.length());
-    //  Serial.println(sensorvalue2.length());
-    // Serial.println(sensorvalue2);
-
-    client.println("Connection: close" );
-    client.println("Host: 157.253.210.190" );//ur web server
-    client.println("Content-Type: application/json");
-    String cont = "Content-Length: ";
-    cont += sizeof(valor);
-    client.println(cont);
-    client.println();
-    client.println(valor);
-    //  client.get("localhost/insert.php?sensor1=20&&sensor2=21");
-  }
-  else
-  {
-    Serial.println("connection failed");
-  }
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    delay(10000);
-  }
+  /*float t = dht.readTemperature();
+  //Serial.print("0 ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  delay(1000);*/
 }
+
---
+++ colmines/tests/TestModels.py
@@ -0,0 +1,143 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .models import Ubicacion,Tipo,Alerta,Rango,Sensor,SubReporte,Reporte,Usuarios
+
+
+class TestUbicacion(TestCase):
+
+    # ran before each test.
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+
+class TestTipo(TestCase):
+    
+    def setUp(self):
+        self.tipo = Tipo(10,"Temperatura")
+    
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.nombre, "Temperatura")
+
+#Falta arreglar de aquí para abajo
+class TestAlerta(TestCase):
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+
+class TestRango(TestCase):
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+
+class TestSensor(TestCase):
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+
+class TestSubReporte(TestCase):
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+
+class TestReporte(TestCase): 
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
+    
+class TestUsuarios(TestCase):
+    
+    def setUp(self):
+        self.ubicacion = Ubicacion(10,10,10,10)
+        self.tipo = Tipo(10,"Temperatura")
+        self.alerta = Alerta(10)
+        self.rango = Rango(10,10,20);
+        self.sensor = Sensor(10,10,"ACTIVO")
+        self.subreporte = SubReporte(0,10,5,10,self.sensor)
+        self.reporte = Reporte("ANOTA")
+        self.usuario = Usuarios("Juan","ADMIN","1234","AT11",self.reporte)
+
+    # test that detail page returns a 200 if the item exists
+    def testCreateUbicacion(self):
+        self.assertEqual(self.ubicacion.id, 10)
+        self.assertEqual(self.ubicacion.zona, 10)
+        self.assertEqual(self.ubicacion.area, 10)
+        self.assertEqual(self.ubicacion.nivel, 10)
