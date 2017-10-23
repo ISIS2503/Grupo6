e0a25a7680528dfd00a7cb77a9764d84d81d118f
tf-venegas10 <tf.vengas10@uniandes.edu.co>
2017-10-23 14:10:05 -0500

    add threading para cada mensaje

--- chequeo/chequeoOnDrugs.py
+++ chequeo/chequeoOnDrugs.py
@@ -1,11 +1,12 @@
 import json
+import threading
 
 from kafka import KafkaConsumer
 
 from chequeo import States
 
-totalSensores=10000
-numeroRangos=8
+totalSensores=2500
+numeroRangos=1
 
 micros=[]
 
@@ -16,17 +17,34 @@ rango=1
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer('normal.'+'rango'+str(rango),group_id='my-group',bootstrap_servers=['172.24.42.23:8090'])
 
+class AgregadorThread(threading):
+    def __init__(self,id,temperatura,gas,ruido,luz):
+        self.id=id
+        self.temperatura=temperatura
+        self.gas=gas
+        self.ruido=ruido
+        self.luz=luz
+    def run(self):
+        micros[self.id].agregarTemperatura(self.temperatura)
+        micros[self.id].agregarGas(self.gas)
+        micros[self.id].agregarRuido(self.ruido)
+        micros[self.id].agregarLuz(self.luz)
+
+
 
 init=totalSensores/numeroRangos*(rango-1)
 for i in range(int(totalSensores/numeroRangos)):
     micros.append(States.Sensor(init + i))
+
 for message in consumer:
     jsonVal=json.loads(message.value)
-    micros[jsonVal['id']].agregarTemperatura(jsonVal['temperatura'])
-    micros[jsonVal['id']].agregarGas(jsonVal['gas'])
-    micros[jsonVal['id']].agregarRuido(jsonVal['ruido'])
-    micros[jsonVal['id']].agregarLuz(jsonVal['luz'])
-
+    id=jsonVal['id']
+    temperatura=jsonVal['temperatura']
+    gas=jsonVal['gas']
+    ruido=jsonVal['ruido']
+    luz=jsonVal['luz']
+    a=AgregadorThread(id,temperatura,gas,ruido,luz)
+    a.start()
 
 
 
