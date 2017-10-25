6dadb4c2544f8daf34835e695eb4c62bde8dc1f8
TOMAS FELIPE VENEGAS BERNAL <tf.venegas10@sis.virtual.uniandes.edu.co>
2017-10-25 15:22:35 -0500

    arreglo chequeo

--- chequeo/States.py
+++ chequeo/States.py
@@ -5,79 +5,79 @@ import json
 from kafka import KafkaProducer
 #clase de objeto de los que heredan sensor y actuador
 
-producer = KafkaProducer(bootstrap_servers=['172.24.42.23:8090'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
+producer = KafkaProducer(bootstrap_servers=['172.24.42.46:8090'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
 ip="172.24.42.40"
 
 class Sensor():
-    def __init__(self,id):
-        self.actuador=Actuador(id)
-        self.ciclo=0
-        self.id=id
-        ya=time.time()
-        self.tiempoTemperatura=ya
-        self.tiempoGas=ya
-        self.tiempoLuz=ya
-        self.tiempoRuido=ya
-        self.estadoTemperatura=Normal()
-        self.estadoGas=Normal()
-        self.estadoLuz=Normal()
-        self.estadoRuido=Normal()
-        self.temperaturas=[]
-        self.gases=[]
-        self.luces=[]
-        self.ruidos=[]
-        self.chequearConexion()
+	def __init__(self,id):
+		self.actuador=Actuador(id)
+		self.ciclo=0
+		self.id=id
+		ya=time.time()
+		self.tiempoTemperatura=ya
+		self.tiempoGas=ya
+		self.tiempoLuz=ya
+		self.tiempoRuido=ya
+		self.estadoTemperatura=Normal()
+		self.estadoGas=Normal()
+		self.estadoLuz=Normal()
+		self.estadoRuido=Normal()
+		self.temperaturas=[]
+		self.gases=[]
+		self.luces=[]
+		self.ruidos=[]
+		self.chequearConexion()
 
-    def chequearConexion(self):
-        ya=time.time()
-        if(ya-self.tiempoGas>=300):
-            self.goState(self.estadoGas,0,350,"")
-        if(ya-self.tiempoRuido>=300):
-            self.goState(self.estadoRuido,0,350,"")
-        if(ya-self.tiempoLuz>=300):
-            self.goState(self.estadoLuz,0,350,"")
-        if(ya-self.tiempoTemperatura>=300):
-            self.goState(self.estadoTemperatura,0,350,"")
-        threading.Timer(300.0,self.chequearConexion).start()
+	def chequearConexion(self):
+		ya=time.time()
+		if(ya-self.tiempoGas>=300):
+			self.goState(self.estadoGas,0,350,"")
+		if(ya-self.tiempoRuido>=300):
+			self.goState(self.estadoRuido,0,350,"")
+		if(ya-self.tiempoLuz>=300):
+			self.goState(self.estadoLuz,0,350,"")
+		if(ya-self.tiempoTemperatura>=300):
+			self.goState(self.estadoTemperatura,0,350,"")
+		threading.Timer(300.0,self.chequearConexion).start()
 
-    def calcularPomedio(self, lista, valor, tiempo):
-        if(valor==None):
-            return sum(lista)/len(lista)
-        if (len(lista)!=10):
-            lista.append(valor)
-            promedio=sum(lista)/len(lista)
-        else:
-            lista.pop(0)
-            lista.append(valor)
-            promedio=sum(lista)/10
-        tiempo=time.time()
-        return promedio
+	def calcularPomedio(self, lista, valor, tiempo):
+		if(valor==None):
+			return sum(lista)/len(lista)
+		if (len(lista)!=10):
+			lista.append(valor)
+			promedio=sum(lista)/len(lista)
+		else:
+			lista.pop(0)
+			lista.append(valor)
+			promedio=sum(lista)/10
+		tiempo=time.time()
+		return promedio
 
-    def agregarTemperatura(self,valor):
-        promedio=self.calcularPomedio(self.temperaturas,valor,self.tiempoTemperatura)
-        self.goState(self.estadoTemperatura,promedio,0,"temperatura")
-    def agregarLuz(self,valor):
-        self.goState(self.estadoLuz, self.calcularPromedio(self.luces,valor,self.tiempoLuz,0,"luz"))
-    def agregarRuido(self,valor):
-        self.goState(self.estadoRuido, self.calcularPromedio(self.ruidos,valor,self.tiempoRuido,0,"ruido"))
-    def agregarGas(self,valor):
-        self.goState(self.estadoGas, self.calcularPromedio(self.gases,valor,self.tiempoGas,0),0,"gas")
+	def agregarTemperatura(self,valor):
+		promedio=self.calcularPomedio(self.temperaturas,valor,self.tiempoTemperatura)
+		self.goState(self.estadoTemperatura,promedio,0,"temperatura")
+	def agregarLuz(self,valor):
+		self.goState(self.estadoLuz, self.calcularPromedio(self.luces,valor,self.tiempoLuz,0,"luz"))
+	def agregarRuido(self,valor):
+		self.goState(self.estadoRuido, self.calcularPromedio(self.ruidos,valor,self.tiempoRuido,0,"ruido"))
+	def agregarGas(self,valor):
+		self.goState(self.estadoGas, self.calcularPromedio(self.gases,valor,self.tiempoGas,0),0,"gas")
 
-    def cicloActuador(self,estado):
-        self.ciclo=self.ciclo+1
-        if(estado.__class__!=FueraDeRango):
-            self.actuador.cambiarEstado()
-            self.ciclo=0
-        if self.ciclo==6:
-            self.ciclo=0
-            self.actuador.malFuncionamiento()
-        threading.Timer(600.0,self.cicloActuador,estado).start()
+	def cicloActuador(self,estado):
+		self.ciclo=self.ciclo+1
+		if(estado.__class__!=FueraDeRango):
+			self.actuador.cambiarEstado()
+			self.ciclo=0
+		if self.ciclo==6:
+			self.ciclo=0
+			self.actuador.malFuncionamiento()
+		threading.Timer(600.0,self.cicloActuador,estado).start()
 
-    def goState(self, estado, promedio,diferenciaTiempo,tipo):
-        estado=estado.goState(promedio,diferenciaTiempo,tipo,self.id)
-        if(estado.__class__==FueraDeRango):
-            self.actuador.cambiarEstado()
-            self.cicloActuador(estado)
+	def goState(self, estado, promedio,diferenciaTiempo,tipo):
+		estado=estado.goState(promedio,diferenciaTiempo,tipo,self.id)
+		if(estado.__class__==FueraDeRango):
+			self.actuador.cambiarEstado()
+			self.cicloActuador(estado)
 
 
 
--- chequeo/chequeoOnDrugs.py
+++ chequeo/chequeoOnDrugs.py
@@ -1,12 +1,13 @@
 import json
 import threading
+import time
 
 
 from kafka import KafkaConsumer
 
 import States
 
-totalSensores=2500
+totalSensores=500
 numeroRangos=1
 
 micros=[]
@@ -17,41 +18,45 @@ rango=1
 
 
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('normal.'+'rango'+str(rango),group_id='my-group',bootstrap_servers=['172.24.42.23:8090'])
+consumer = KafkaConsumer('normal.'+'rango'+str(rango),group_id='my-group',bootstrap_servers=['172.24.42.46:8090'])
 sem= threading.BoundedSemaphore()
 promedio = 1
 valorPrueba = 1500
 i=0
 
 class AgregadorThread(threading.Thread):
-
-    global promedio
-    global i
-    def __init__(self,id,temperatura,gas,ruido,luz,time):
-        threading.Thread.__init__(self)
-        self.id=id
-        self.temperatura=temperatura
-        self.gas=gas
-        self.ruido=ruido
-        self.luz=luz
-        self.time=time
-    def run(self):
-        micros[self.id].agregarTemperatura(self.temperatura)
-        micros[self.id].agregarGas(self.gas)
-        micros[self.id].agregarRuido(self.ruido)
-        micros[self.id].agregarLuz(self.luz)
-        sem.acquire()
-        self.promedio= (self.time-time.time()+self.promedio*self.i)/(self.i+1)
-        sem.release()
-        sem.notify()
-
+	def __init__(self,id,temperatura,gas,ruido,luz,time):
+		threading.Thread.__init__(self)
+		self.id=id
+		self.temperatura=temperatura
+		self.gas=gas
+		self.ruido=ruido
+		self.luz=luz
+		self.time=time
+	def run(self):
+		global promedio
+		global i
+		micros[self.id].agregarTemperatura(self.temperatura)
+		micros[self.id].agregarGas(self.gas)
+		micros[self.id].agregarRuido(self.ruido)
+		micros[self.id].agregarLuz(self.luz)
+		sem.acquire()
+		promedio= (self.time-time.time()+promedio*i)/(i+1)
+		sem.release()
+		sem.notify()
 
 
 
 
-init=totalSensores/numeroRangos*(rango-1)
-for i in range(int(totalSensores/numeroRangos)):
-    micros.append(States.Sensor(init+i))
+
+#init=totalSensores/numeroRangos*(rango-1)
+init=0
+print("creando objetos")
+t=time.time()
+for i in range(totalSensores):
+	micros.append(States.Sensor(init+i))
+	print(i)
+print("Objetos creados tiempo tomado: "+str(time.time()-t))
 
 for message in consumer:
     print(message)
@@ -65,9 +70,11 @@ for message in consumer:
     a=AgregadorThread(id,temperatura,gas,ruido,luz,time)
     a.start()
     sem.acquire()
-    while(i<valorPrueba):
-        sem.wait()
-    print(str(i)+" :"+ str(promedio))
+while i<valorPrueba :
+	sem.release()
+	time.sleep(1)
+	sem.acquire()
+print(str(i)+" :"+ str(promedio))
 
 
 
