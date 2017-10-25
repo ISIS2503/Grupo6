a090892e539a0f7f0907d22248259f84883debcc
Juan Diego <jd.gonzalza@uniandes.edu.co>
2017-10-24 20:21:27 -0500

    arreglos

--- colmines/dashboard/urls.py
+++ colmines/dashboard/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     url(r'^tipos/$', views.tipo_list),
     url(r'^tipos/(?P<pk>[0-9]+)/$', views.tipo_detail),
 
-    url(r'^sensores/$', views.sensor_list),
-    url(r'^sensores/(?P<pk>[0-9]+)/$', views.sensor_detail),
+    url(r'^micro/$', views.micro_list),
+    url(r'^micro/(?P<pk>[0-9]+)/$', views.micro_detail),
 
     url(r'^alertas/$', views.alerta_list),
     url(r'^alertas/(?P<pk>[0-9]+)/$', views.alerta_detail),
 
     url(r'^mediciones/$', views.medicion_list),
-    url(r'^mediciones/(?P<pk>[0-9]+)/$', views.medicion_detail),    
+    url(r'^mediciones/(?P<pk>[0-9]+)/$', views.medicion_detail),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
